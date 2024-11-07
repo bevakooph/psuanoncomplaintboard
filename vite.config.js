e335cd74d0aeeec7a5b4ac8b63570cd5dbf95c91
sparsh <kapoor.sparsh@outlook.com>
2024-11-07 16:22:15 -0500

    404 html routing

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true
-  }
+  base: '/psuanoncomplaintboard/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
