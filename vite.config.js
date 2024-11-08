f8f381e742e8dfd749ae20ba3e7919b28026d859
sparsh <kapoor.sparsh@outlook.com>
2024-11-08 11:23:38 -0500

    updated vite for firebase

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/psuanoncomplaintboard/',
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      external: ['firebase/app', 'firebase/firestore']
+    }
   }
 })
